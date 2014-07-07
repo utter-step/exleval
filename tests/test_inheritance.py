91786670300f8c0ebe8d649fc0db454bee52cb3b
Vlad Stepanov <8uk.8ak@gmail.com>
2014-07-07 16:27:55 +0400

    One more test case + example

--- tests/test_inheritance.py
+++ tests/test_inheritance.py
@@ -35,8 +35,36 @@ class ListComprehensionEvaler(Evaler):
         "[10 - x for x in some_iterable]",
     )
 
+
+class OnlyNamesAndAdditionEvaler(Evaler):
+    ALLOWED_NODES = set((
+        _ast.Module,
+        _ast.Expr,
+
+        _ast.BinOp,
+        _ast.Add,
+
+        _ast.Num,
+        _ast.Name,
+        _ast.Load,
+    ))
+
+    def get_allowed_nodes(self):
+        return self.ALLOWED_NODES
+
+    test_code = (
+        ("4 + 3", True),
+        ("x + 2", True),
+        ("5 + x", True),
+        ("4 - 3", False),
+        ("x - 5", False),
+        ("x / 4", False),
+    )
+
+
 nb_evaler = NoBinaryEvaler()
 lc_evaler = ListComprehensionEvaler((range, xrange))
+ona_evaler = OnlyNamesAndAdditionEvaler()
 
 
 @pytest.mark.parametrize('expr', NoBinaryEvaler.bad_code)
@@ -50,6 +78,13 @@ def test_NoBinary(expr):
 
 @pytest.mark.parametrize('expr', ListComprehensionEvaler.good_code)
 def test_ListComprehencion(expr):
-    lc_evaler.eval(expr,
-                   {"some_iterable": (x ** 0.5 for x in xrange(10))}
-                   )
+    lc_evaler.eval(expr, {"some_iterable": (x ** 0.5 for x in xrange(10))})
+
+
+@pytest.mark.parametrize('expr,good', OnlyNamesAndAdditionEvaler.test_code)
+def test_addition_only(expr, good):
+    if good:
+        ona_evaler.eval(expr, {"x": 5})
+    else:
+        with pytest.raises(NotSafeExpression):
+            ona_evaler.eval(expr, {"x": 5})
