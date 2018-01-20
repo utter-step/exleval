b0e32d9d321b4bad9ba641f299af7efa6711ea87
Vlad Stepanov <8uk.8ak@gmail.com>
2018-01-20 18:13:54 +0300

    [infra] setuptools -> setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='exleval',
     author='Vlad Stepanov',
     author_email='8uk.8ak@gmail.com',
-    version='0.1',
+    version='0.1.1',
     package_dir={'': 'src'},
     packages=['exleval'],
     classifiers=[
